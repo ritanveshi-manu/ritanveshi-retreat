9b21a92b0cd093457490838d620697047e623871
ritanveshi-manu <ritanveshi.manu@gmail.com>
2024-12-19 01:29:02 +0530

    Fixing the issue

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@ import i18n from "./translation";
 // };
 
 const App = () => (
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/ritanveshi-retreat">
     <I18nextProvider i18n={i18n}>
       <Router />
     </I18nextProvider>
